27bfb5a6124c91c78bc445883760101804d5a04e
Nikita Lazarev-Zubov <lazarevzubov@devexperts.com>
2019-07-25 13:41:25 +0300

    Re-format

--- ComposedTableViewDataSource.playground/Contents.swift
+++ ComposedTableViewDataSource.playground/Contents.swift
@@ -24,6 +24,148 @@ protocol ComposableTableViewDataSource: UITableViewDataSource {
 
 // MARK: -
 
+/**
+ Data source that can be composed of multiple original data source objects.
+ Rows movements is not supported, updates observation is under implementation.
+ */
+final class ComposedTableViewDataSource: NSObject, UITableViewDataSource {
+    
+    // MARK: - Properties
+    
+    private let dataSources: [ComposableTableViewDataSource]
+    
+    // MARK: - Initialization
+    
+    // MARK: ComposedTableViewDataSource protocol initializers
+    
+    /**
+     Initializes and returns composed data source object.
+     - parameter dataSources: Any number of data source objects to be composed can be passed as an array or comma separated list.
+     */
+    init(dataSources: ComposableTableViewDataSource...) {
+        self.dataSources = dataSources
+        super.init()
+    }
+    
+    private override init() {
+        fatalError("ComposedTableViewDataSource: Initializer with parameters must be used.")
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: ComposedTableViewDataSource protocol methods
+    
+    // Configuring a Table View
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // Default value if not implemented is "1".
+        return dataSources.reduce(0) { $0 + ($1.numberOfSections?(in: tableView) ?? 1) }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return adduce(section) { $0.tableView?(tableView, titleForHeaderInSection: $1) }
+    }
+    
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return adduce(section) { $0.tableView?(tableView, titleForFooterInSection: $1) }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return adduce(section) { $0.tableView(tableView, numberOfRowsInSection: $1) }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return adduce(indexPath) { $0.tableView(tableView, cellForRowAt: $1) }
+    }
+    
+    // Inserting or Deleting Table Rows
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        return adduce(indexPath) { $0.tableView?(tableView, commit: editingStyle, forRowAt: $1) }
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Default if not implemented is "true".
+        return adduce(indexPath) { $0.tableView?(tableView, canEditRowAt: $1) ?? true }
+    }
+    
+    // Reordering Table Rows
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        /*
+         Movement is disabled because different data source objects can operate different object types, hence there's no possibility to move a corresponding object from one data source to another. At this moment there's no way to figure out wether movement occurs within a single data source bounds.
+         */
+        return false
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        /*
+         There's no possibility to interrupt movement within data source object. This method responsibility is to handle movement accordingly row's relocation.
+         */
+    }
+    
+    // Configuring an Index
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return dataSources.reduce([String]()) { $0 + ($1.sectionIndexTitles?(for: tableView) ?? [String]()) }
+    }
+    
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        // TODO: Think about force-unwrapping.
+        return adduceTitleIndex(index) { $0.tableView!(tableView, sectionForSectionIndexTitle: title, at: $1) }
+    }
+    
+    // MARK: Private methods
+    
+    private typealias SectionNumber = Int
+    
+    private typealias AdducedSectionTask<T> = (_ composableDataSource: ComposableTableViewDataSource, _ sectionNumber: SectionNumber) -> T
+    
+    private func adduce<T>(_ section: SectionNumber, _ task: AdducedSectionTask<T>) -> T {
+        let (dataSource, decomposedSection) = decompose(section: section)
+        return task(dataSource, decomposedSection)
+    }
+    
+    private typealias AdducedIndexPathTask<T> = (_ composableDataSource: ComposableTableViewDataSource, _ indexPath: IndexPath) -> T
+    
+    private func adduce<T>(_ indexPath: IndexPath, _ task: AdducedIndexPathTask<T>) -> T {
+        let (dataSource, decomposedSection) = decompose(section: indexPath.section)
+        return task(dataSource, IndexPath(row: indexPath.row, section: decomposedSection))
+    }
+    
+    private func decompose(section: SectionNumber) -> (dataSource: ComposableTableViewDataSource, decomposedSection: SectionNumber) {
+        var section = section
+        var dataSourceIndex = 0
+        for (index, dataSource) in dataSources.enumerated() {
+            let diff = section - dataSource.numberOfSections
+            dataSourceIndex = index
+            if diff < 0 { break } else { section = diff }
+        }
+        
+        return (dataSources[dataSourceIndex], section)
+    }
+    
+    private func adduceTitleIndex<T>(_ sectionTitleIndex: Int, _ task: AdducedSectionTask<T>) -> T {
+        let (dataSource, decomposedSectionTitleIndex) = decompose(sectionTitleIndex: sectionTitleIndex)
+        return task(dataSource, decomposedSectionTitleIndex)
+    }
+    
+    private func decompose(sectionTitleIndex: Int) -> (dataSource: ComposableTableViewDataSource, decomposedSection: SectionNumber) {
+        var titleIndex = sectionTitleIndex
+        var dataSourceIndex = 0
+        for (index, dataSource) in dataSources.enumerated() {
+            let diff = titleIndex - dataSource.numberOfSectionTitles
+            dataSourceIndex = index
+            if diff < 0 { break } else { titleIndex = diff }
+        }
+        
+        return (dataSources[dataSourceIndex], titleIndex)
+    }
+    
+}
+
+// MARK: - Example
+
 struct Book {
     
     // MARK: - Properties
@@ -194,147 +336,7 @@ final class MagazineDataSource: NSObject, ComposableTableViewDataSource {
     
 }
 
-// MARK: -
-
-/**
- Data source that can be composed of multiple original data source objects.
- Rows movements is not supported, updates observation is under implementation.
- */
-final class ComposedTableViewDataSource: NSObject, UITableViewDataSource {
-    
-    // MARK: - Properties
-    
-    private let dataSources: [ComposableTableViewDataSource]
-    
-    // MARK: - Initialization
-    
-    // MARK: ComposedTableViewDataSource protocol initializers
-    
-    /**
-     Initializes and returns composed data source object.
-     - parameter dataSources: Any number of data source objects to be composed can be passed as an array or comma separated list.
-     */
-    init(dataSources: ComposableTableViewDataSource...) {
-        self.dataSources = dataSources
-        super.init()
-    }
-    
-    private override init() {
-        fatalError("ComposedTableViewDataSource: Initializer with parameters must be used.")
-    }
-    
-    // MARK: - Methods
-    
-    // MARK: ComposedTableViewDataSource protocol methods
-    
-    // Configuring a Table View
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        // Default value if not implemented is "1".
-        return dataSources.reduce(0) { $0 + ($1.numberOfSections?(in: tableView) ?? 1) }
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return adduce(section) { $0.tableView?(tableView, titleForHeaderInSection: $1) }
-    }
-    
-    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        return adduce(section) { $0.tableView?(tableView, titleForFooterInSection: $1) }
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return adduce(section) { $0.tableView(tableView, numberOfRowsInSection: $1) }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return adduce(indexPath) { $0.tableView(tableView, cellForRowAt: $1) }
-    }
-    
-    // Inserting or Deleting Table Rows
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        return adduce(indexPath) { $0.tableView?(tableView, commit: editingStyle, forRowAt: $1) }
-    }
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Default if not implemented is "true".
-        return adduce(indexPath) { $0.tableView?(tableView, canEditRowAt: $1) ?? true }
-    }
-    
-    // Reordering Table Rows
-    
-    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        /*
-         Movement is disabled because different data source objects can operate different object types, hence there's no possibility to move a corresponding object from one data source to another. At this moment there's no way to figure out wether movement occurs within a single data source bounds.
-         */
-        return false
-    }
-    
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        /*
-         There's no possibility to interrupt movement within data source object. This method responsibility is to handle movement accordingly row's relocation.
-         */
-    }
-    
-    // Configuring an Index
-    
-    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-        return dataSources.reduce([String]()) { $0 + ($1.sectionIndexTitles?(for: tableView) ?? [String]()) }
-    }
-    
-    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
-        // TODO: Think about force-unwrapping.
-        return adduceTitleIndex(index) { $0.tableView!(tableView, sectionForSectionIndexTitle: title, at: $1) }
-    }
-    
-    // MARK: Private methods
-    
-    private typealias SectionNumber = Int
-   
-    private typealias AdducedSectionTask<T> = (_ composableDataSource: ComposableTableViewDataSource, _ sectionNumber: SectionNumber) -> T
-    
-    private func adduce<T>(_ section: SectionNumber, _ task: AdducedSectionTask<T>) -> T {
-        let (dataSource, decomposedSection) = decompose(section: section)
-        return task(dataSource, decomposedSection)
-    }
-    
-    private typealias AdducedIndexPathTask<T> = (_ composableDataSource: ComposableTableViewDataSource, _ indexPath: IndexPath) -> T
-    
-    private func adduce<T>(_ indexPath: IndexPath, _ task: AdducedIndexPathTask<T>) -> T {
-        let (dataSource, decomposedSection) = decompose(section: indexPath.section)
-        return task(dataSource, IndexPath(row: indexPath.row, section: decomposedSection))
-    }
-    
-    private func decompose(section: SectionNumber) -> (dataSource: ComposableTableViewDataSource, decomposedSection: SectionNumber) {
-        var section = section
-        var dataSourceIndex = 0
-        for (index, dataSource) in dataSources.enumerated() {
-            let diff = section - dataSource.numberOfSections
-            dataSourceIndex = index
-            if diff < 0 { break } else { section = diff }
-        }
-        
-        return (dataSources[dataSourceIndex], section)
-    }
-    
-    private func adduceTitleIndex<T>(_ sectionTitleIndex: Int, _ task: AdducedSectionTask<T>) -> T {
-        let (dataSource, decomposedSectionTitleIndex) = decompose(sectionTitleIndex: sectionTitleIndex)
-        return task(dataSource, decomposedSectionTitleIndex)
-    }
-    
-    private func decompose(sectionTitleIndex: Int) -> (dataSource: ComposableTableViewDataSource, decomposedSection: SectionNumber) {
-        var titleIndex = sectionTitleIndex
-        var dataSourceIndex = 0
-        for (index, dataSource) in dataSources.enumerated() {
-            let diff = titleIndex - dataSource.numberOfSectionTitles
-            dataSourceIndex = index
-            if diff < 0 { break } else { titleIndex = diff }
-        }
-        
-        return (dataSources[dataSourceIndex], titleIndex)
-    }
-    
-}
+// MARK: Specific example
 
 let book0 = Book(autor: "Anne Brontë", name: "The Tenant of Wildfell Hall")
 let book1 = Book(autor: "Charlotte Brontë", name: "Jane Eyre")
